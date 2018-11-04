f0398a915dd6c34c121d4dd50246559b43f314fe
Tijs Moree <tijsmoree@gmail.com>
2018-11-04 18:12:21 +0100

    Started some changes

--- Exercises-2/fixit.c
+++ Exercises-2/fixit.c
@@ -12,11 +12,11 @@
 int main (int argc, char *argv[]) 
 {
 int nthreads, tid, i, j;
-double a[N][N];
 
 /* Fork a team of threads */
-#pragma omp parallel shared(nthreads) private(i,tid,a)
+#pragma omp parallel shared(nthreads) private(i,tid)
   {
+  double *a = malloc(N * N * sizeof(double)); 
 
   /* Obtain/print thread info */
   tid = omp_get_thread_num();
@@ -30,10 +30,10 @@ double a[N][N];
   /* Each thread works on its own private copy of the array */
   for (i=0; i<N; i++)
     for (j=0; j<N; j++)
-      a[i][j] = tid + i + j;
+      a[i * N + j] = tid + i + j;
 
   /* For confirmation */
-  printf("Thread %d done. Last element= %lf\n",tid,a[N-1][N-1]);
+  printf("Thread %d done. Last element= %lf\n",tid,a[N * N - 1]);
      /* %d, %lf - print a decimal number and a long floating (double) number repectively */
 
   }  /* All threads join master thread and disband */
